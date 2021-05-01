9c28d70c59c813d11acc612968706203c698f26b
pedro torchio <pedro@frankiefinancial.com>
2021-05-01 20:01:15 +1000

    including colors.sass in configuration directory

---
+++ config/colors.sass.d.ts
@@ -0,0 +1,11 @@
+interface Colors {
+  INFO: string;
+  WARNING: string;
+  DANGER: string;
+  SUCCESS: string;
+  MAIN: string;
+  FONT: string;
+  FADE: string;
+}
+const styles: Colors;
+export default styles;
