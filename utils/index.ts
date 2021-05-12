84fad311264d5bbbb1f39bf97fd4e8bb41c1ac46
pedro torchio <pedro@frankiefinancial.com>
2021-05-12 23:56:55 +1000

    exporting types

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,4 @@
 export { default as debounce } from './debounce';
 export * from './debounce';
 export * from './activityFieldUIMetadata';
-export * from './sessionStorage';
 export * from './time';
