e40b07cc699c525b46f3f68bee7d5315a55a0586
Pedro Torchio <pedrotorchio@gmail.com>
2022-01-24 18:40:46 +1100

    exposing events subscriber

--- EventsSubscriber/EventsSubscriber.ts
+++ EventsSubscriber/EventsSubscriber.ts
@@ -1,16 +1,23 @@
-type Callback = (...args: any[]) => void;
-type EventsDictionary = {
+export type Callback = (...args: any[]) => void;
+export type EventsDictionary = {
   [k: string]: Callback[];
 }
 export class EventsSubscriber<Events extends EventsDictionary> {
   private _listeners = {} as Events;
   
-  listen<Event extends keyof Events>(event: Event, cb: Events[Event][0]): void {
+  listen<Event extends keyof Events>(event: Event, cb: Events[Event][0]): string {
     this._listeners[event].push(cb);
+    return `${event}.${this._listeners[event].length - 1}`;
   }
   publish<Event extends keyof Events>(event: Event, ...parameters: Parameters<Events[Event][0]>): void {
     const allListeners: Callback[] = this._listeners[event];
     allListeners.forEach(listen => listen(...parameters));
   }
+  unlisten(code: string): void {
+    const [event, indexstr] = code.split('.')
+    const index = parseInt(indexstr);
+    if (!event || isNaN(index)) throw new Error(`Invalid unlisten code (${code})`);
 
+    this._listeners[event].splice(index, 1);
+  }
 }
--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export * from './models';
 export * from './utils';
 export * from './config';
 export * from './types';
+export * from './EventsSubscriber';
