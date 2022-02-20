18417113153daff6c4618eb5a69141fca97dc654
Pedro Torchio <pedrotorchio@gmail.com>
2022-02-20 12:23:05 +1100

    adding request counter into http client object

--- api/HttpClient.ts
+++ api/HttpClient.ts
@@ -2,17 +2,33 @@ import Axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import { EventsSubscriber } from '../EventsSubscriber/EventsSubscriber';
 
 const hasStatus = (error: AxiosError, st: number) => error?.response?.status == st;
-export function createNewHttpClient(BASE_URL: string): { client: AxiosInstance, eventsSubscriber: EventsSubscriber<{}>, setSessionToken: (t: string) => void } {
+type HttpClientInfo = { 
+  client: AxiosInstance, 
+  eventsSubscriber: EventsSubscriber<{}>, 
+  setSessionToken: (t: string) => void,
+  requestCounter: { active: number }
+};
+export function createNewHttpClient(BASE_URL: string): HttpClientInfo {
   const eventsSubscriber = new EventsSubscriber();
   const axiosInstance = Axios.create({
     baseURL: BASE_URL
   });
-
+  const requestCounter = { active: 0 };
+  axiosInstance.interceptors.request.use((c) => {
+    requestCounter.active++;
+    eventsSubscriber.publish('request', { configuration: c });
+    return c;
+  })
+  
   axiosInstance.interceptors.response.use((r: AxiosResponse) => {
+    requestCounter.active--;
+    eventsSubscriber.publish('response', { response: r });
     const token = r.headers.token;
     if (token) setSessionToken(token);
     return r;
   }, (error: AxiosError) => {
+    requestCounter.active--;
+    eventsSubscriber.publish('error-response', { error });
     console.error(error);
     if (hasStatus(error, 401)||hasStatus(error, 403)) clearToken();
     throw error;
@@ -28,6 +44,7 @@ export function createNewHttpClient(BASE_URL: string): { client: AxiosInstance, 
   return {
     setSessionToken,
     client: axiosInstance,
-    eventsSubscriber
+    eventsSubscriber,
+    requestCounter
   }
 }
