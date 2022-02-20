ca491b52add89440a535e10d2d4373b1ea850d46
Pedro Torchio <pedrotorchio@gmail.com>
2022-02-20 13:06:53 +1100

    adding active request counter to http client

--- api/HttpClient.ts
+++ api/HttpClient.ts
@@ -1,15 +1,22 @@
-import Axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import Axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { EventsSubscriber } from '../EventsSubscriber/EventsSubscriber';
 
 const hasStatus = (error: AxiosError, st: number) => error?.response?.status == st;
 type HttpClientInfo = { 
   client: AxiosInstance, 
-  eventsSubscriber: EventsSubscriber<{}>, 
+  eventsSubscriber: EventsSubscriber<Events>, 
   setSessionToken: (t: string) => void,
   requestCounter: { active: number }
 };
+type Events = {
+  request: (o: {configuration: AxiosRequestConfig}) => void;
+  response: (o: {response: AxiosResponse}) => void;
+  'error-response': (o: {error: AxiosError}) => void;
+  'new-token': (t: string) => void;
+  'clear-token': () => void;
+}
 export function createNewHttpClient(BASE_URL: string): HttpClientInfo {
-  const eventsSubscriber = new EventsSubscriber();
+  const eventsSubscriber = new EventsSubscriber<Events>();
   const axiosInstance = Axios.create({
     baseURL: BASE_URL
   });
@@ -36,10 +43,10 @@ export function createNewHttpClient(BASE_URL: string): HttpClientInfo {
 
   function setSessionToken(token: string) {
     axiosInstance.defaults.headers["token"] = token;
-    eventsSubscriber.publish("newToken", token);
+    eventsSubscriber.publish("new-token", token);
   }
   function clearToken() {
-    eventsSubscriber.publish("clearToken");
+    eventsSubscriber.publish("clear-token");
   }
   return {
     setSessionToken,
